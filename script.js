25d302576b2f42bff79416579c8e0efa29757f3e
Chiraqdev <107565092+Chiraqdev@users.noreply.github.com>
2024-10-21 23:22:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault(); // Empêche la soumission normale du formulaire
     
-    const webhookUrl = 'https://discord.com/api/webhooks/1298030387439734824/4_-i0OxRlmzHWWB4KWlFhsBaOI9z9bLlbLQiTVpnfH3CX0JeGA5jK0fbfLN2w3HMHxm4'; // Colle ton Webhook ici
-    
+    const webhookUrl = 'https://discord.com/api/webhooks/1298030387439734824/4_-i0OxRlmzHWWB4KWlFhsBaOI9z9bLlbLQiTVpnfH3CX0JeGA5jK0fbfLN2w3HMHxm4'; // Remplace par ton Webhook
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
-    
-    // Format du message à envoyer à Discord
+
     const payload = {
         content: `Nouveau message de contact :\nNom : ${name}\nEmail : ${email}\nMessage : ${message}`
     };
-    
-    // Envoie les données à Discord
+
     fetch(webhookUrl, {
         method: 'POST',
         headers: {
